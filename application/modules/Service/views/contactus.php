e2a27a653b12788bdaa08de14d23da76321fc994
DESKTOP-AOL2S06\itsupport <fashahd@gmail.com>
2018-01-26 15:03:31 +0700

    major update

---
+++ application/modules/Service/views/contactus.php
@@ -0,0 +1,75 @@
+<!-- breadcrumb-area-start -->
+<div class="breadcrumb-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="breadcrumb-content text-center">
+					<div class="breadcrumb-title text-left">
+						<h3><a href="#">We're Here To Help.</a></h3>
+						<p style="font-weight:700;margin-top:10px">Use the form below to search for an authorized RYU service center near you.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="breadcrumb-area-down">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12" style="margin-top:30px">
+				<div class="breadcrumb-content-down">					
+					<div class="col-lg-12 col-xs-12">
+                        <div class="user-login-area mb-70">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12">
+                                    </div>
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                        <div class="row">
+                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                <div class="single-register-2">
+                                                    <form action="#">
+                                                        <label>First Name<span>*</span></label>
+                                                        <input type="text"/>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                <div class="single-register-2">
+                                                    <form action="#">
+                                                        <label>First Name<span>*</span></label>
+                                                        <input type="text"/>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                <div class="single-register-2">
+                                                    <form action="#">
+                                                        <label>First Name<span>*</span></label>
+                                                        <input type="text"/>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                <div class="single-register-2">
+                                                    <form action="#">
+                                                        <label>First Name<span>*</span></label>
+                                                        <input type="text"/>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+</div>
---
+++ application/modules/Service/views/loan.php
@@ -0,0 +1,34 @@
+<!-- breadcrumb-area-start -->
+<div class="breadcrumb-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="breadcrumb-content text-center">
+					<div class="breadcrumb-title text-left">
+						<h3><a href="#">Loan Programe</a></h3>
+						<p style="font-weight:700;margin-top:10px">Use the form below to search for an authorized RYU service center near you.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="breadcrumb-area-down">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12" style="margin-top:30px">
+				<div class="breadcrumb-content-down">					
+					<div class="col-lg-12 col-xs-12">
+						<h2>Loan Programe</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="warranty">
+		<h4>General Warranty Information</h4>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		<h4>General Warranty Information</h4>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	</div>
+</div>
